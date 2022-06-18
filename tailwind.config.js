fadf5e62fa3a0e05a7341c777b405147009f0f67
davidteren <drteren@gmail.com>
2022-06-18 14:51:23 +0200

    wip

--- tailwind.config.js
+++ tailwind.config.js
@@ -27,16 +27,42 @@ module.exports = {
         '9xl': '9rem',
       },
       colors: {
-        bg: '#0f172a',
-        'accent-bg': '#1e293b',
-        text: '#f8fafc',
-        'text-light': '#f1f5f9',
-        border: '#cbd5e1',
-        accent: '#0ea5e9',
-        code: '#ef4444',
-        preformatted: '#94a3b8',
-        marked: '#fdba74',
-        disabled: '#94a3b8',
+        //Slate
+        // bg: '#0f172a',
+        // 'accent-bg': '#1e293b',
+        // text: '#f8fafc',
+        // 'text-light': '#f1f5f9',
+        // border: '#cbd5e1',
+        // accent: '#0ea5e9',
+        // code: '#ef4444',
+        // preformatted: '#94a3b8',
+        // marked: '#fdba74',
+        // disabled: '#94a3b8',
+
+        // Simplecss dark theme
+        bg: '#212121',
+        'accent-bg': '#2b2b2b',
+        text: '#dcdcdc',
+        'text-light': '#ababab',
+        border: '#666',
+        accent: '#ffb300',
+        code: '#f06292',
+        preformatted: '#ccc',
+         marked: '#ffdd33',
+        disabled: '#111',
+
+        // Simplecss light theme
+        // bg: '#fff',
+        // 'accent-bg': '#f5f7ff',
+        // text: '#212121',
+        // 'text-light': '#585858',
+        // border: '#d8dae1',
+        // accent: '#0d47a1',
+        // code: '#d81b60',
+        // preformatted: '#444',
+        // marked: '#ffdd33',
+        // disabled: '#efefef',
+
       }
     }
   },
